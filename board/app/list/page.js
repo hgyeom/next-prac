9a0da48deea6d292150ec22f624bbe82f6e4d1de
hgyeom <rrune3@naver.com>
2024-02-14 14:33:47 +0900

    다이나믹 렌더링, 페이지 캐싱

--- board/app/list/page.js
+++ board/app/list/page.js
@@ -2,6 +2,12 @@ import { connectDB } from '@/util/database';
 import ListItem from './listItem';
 import GoWritePage from './goWritePage';
 
+// 다이나믹 렌더링
+export const dynamic = 'force-dynamic';
+
+// 페이지 단위 캐싱. 기존 넥스트의 ISR
+export const revalidate = 20;
+
 export default async function List() {
   const db = (await connectDB).db('forum');
   const result = await db.collection('post').find().toArray();
